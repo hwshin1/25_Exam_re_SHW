6d63649711f8c36b48d978d15e842a5f36eae91a
hwshin1 <hwshin5044@gmail.com>
2025-03-26 17:32:58 +0900

    피보나치

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,18 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // 양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다.
-        // 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소
-        Scanner sc = new Scanner(System.in);
+        // 프로그램은 정수 10개의 피보나치 수열 숫자를 출력합니다.
+        int n = 10;
+        int f = 0;
+        int s = 1;
 
-        System.out.print("숫자 입력 : ");
-        int n = sc.nextInt(); // n = 5를 넣어본다
+        for (int i = 1; i <= n; ++i) { // n 변수인 10을 넣고 10번 돌리는데 1증가를 먼저 하고 돌린다.
+            System.out.print(f + " "); // 공백 있는 상태로 f를 출력
 
-        for (int i = n; i >= 1; i--) { // 역순으로 n번까지 돌린다.
-            for (int j = n + 1; j >= (i*2) - 2; j-=2) { // -2씩 감소함 i가 for문을 돌아 순차적으로 감소
-                System.out.print(" "); // 공백이 2개 생성됨
-            }
-            for (int k = i; k >= 1; k--) {
-                System.out.print(" " + k); // 숫자 역순으로 출력 각 숫자 왼쪽에 공백도 추가
-            }
-            System.out.println(); // 안에 있는 for문이 한번 돌려졌을 때 줄바꿈을 해준다.
+            int next = f + s; // int next라는 변수에 f값과 s값을 더해서 넣는다.
+            f = s; // f에 s값을 넣는다.
+            s = next; // s에 f+s를 넣은 next 값을 넣는다.
         }
     }
 }
