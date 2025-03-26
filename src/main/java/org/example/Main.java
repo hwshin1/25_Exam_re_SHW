f981aa39a754fe4bcd5ad7649ed69d41f9e2c379
hwshin1 <hwshin5044@gmail.com>
2025-03-26 17:25:59 +0900

    양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현
+        // 양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다.
+        // 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소
         Scanner sc = new Scanner(System.in);
 
         System.out.print("숫자 입력 : ");
-        int cols = sc.nextInt();
-        System.out.print("숫자 입력 : ");
-        int rows = sc.nextInt();
+        int n = sc.nextInt(); // n = 5를 넣어본다
 
-        for (int i = 1; i <= rows; i++) { // rows의 변수에 들어간 값 까지 반복문을 돌린다.
-            for (int j = 1; j <= cols; j++) { // cols의 변수에 들어간 값으로 반복문 돌린다.
-                System.out.print("*"); // * 모양으로 출력한다.
+        for (int i = n; i >= 1; i--) { // 역순으로 n번까지 돌린다.
+            for (int j = n + 1; j >= (i*2) - 2; j-=2) { // -2씩 감소함 i가 for문을 돌아 순차적으로 감소
+                System.out.print(" "); // 공백이 2개 생성됨
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(" " + k); // 숫자 역순으로 출력 각 숫자 왼쪽에 공백도 추가
             }
             System.out.println(); // 안에 있는 for문이 한번 돌려졌을 때 줄바꿈을 해준다.
         }
